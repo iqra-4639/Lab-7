95bc06098ce468399ae32212b2fecd709a78d3af
iqra-4639 <iqra.bsse4639@student.iiu.edu.pk>
2026-04-05 17:29:33 +0500

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,7 +3,7 @@ import unittest
 class TestApp(unittest.TestCase):
 
     def test_addition(self):
-        self.assertEqual(2 + 3, 6)
+        self.assertEqual(2 + 3, 5)
 
 if __name__ == "__main__":
     unittest.main()
